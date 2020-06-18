6760d8718b2a6b1565b0e3aa02e9e32d1649a09c
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-06-18 15:26:45 +0700

    Khởi tạo dự án

---
+++ src/test/java/com/codegym/education/EducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
